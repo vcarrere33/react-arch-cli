0a6366cbd79b3dce8592bf4449e27b9443fe8d5c
matthieu-brillaxis <matthieu.brillaxis@live.com>
2019-02-14 09:46:53 +0100

    create script for action, reducer, saga

--- src/createAction.js
+++ src/createAction.js
@@ -1,13 +1,49 @@
+const shell = require('shelljs');
+const template = require('./../template/actionTemplate');
+const fs = require('fs-extra');
+const replace = require('replace');
+const colors = require('colors');
 
-export default function createAction(component, cmd) {
-  newCompPath = component;
-  cmd.nofolder ? nofolder = true : nofolder = false;
-  cmd.functional ? functional = true : functional = false;
-  cmd.observable ? observable = true : observable = false;
-  cmd.style ? stylesheet = true : stylesheet = false;
-  if (fs.existsSync('./src/components')) {
-    newCompPath = `./src/components/${component}`;
+module.exports = async function createAction(action, cmd) {
+  newActionPath = action;
+  if (!fs.existsSync('./src/action')) {
+    fs.mkdir('./src/action', { recursive: true }, (err) => {
+      if (err) console.log(err);
+      else console.log('Folder action created');
+    });
   }
-  let template = await buildTemplate();
-  writeFile(template, component)
+  let template = await buildTemplateAction();
+  writeFile(template, action, newActionPath);
 };
+
+function buildTemplateAction() {
+  let imports = [template.imports.flow];
+
+  let body = [template.main].join('\n');
+  let exported = [template.exported.default];
+  return imports.join('\n') + '\n' + body + '\n' + exported;
+}
+
+function capitalize(comp) {
+  return comp[0].toUpperCase() + comp.substring(1, comp.length);
+}
+
+function writeFile(template, action, path) {
+  let act = action.split('/');
+  act = act[act.length - 1];
+  if (!fs.existsSync(`${path}.js`)) {
+    fs.outputFile(`${path}Action.js`, template, (err) => {
+      if (err) throw err;
+      replace({
+        regex: ":className",
+        replacement: capitalize(redu),
+        paths: [`${path}Action.js`],
+        recursive: false,
+        silent: true,
+      });
+      console.log(`Action ${redu} created at ${path}Action.js`.cyan)
+    });
+  } else {
+    console.log(`Action ${redu} allready exists at ${path}Action.js, choose another name if you want to create a new action`.red)
+  }
+}
--- src/createComponent.js
+++ src/createComponent.js
@@ -1,6 +1,8 @@
-import shell from 'shelljs';
-import template from './../template/componentTemplate';
-import fs from 'fs-extra';
+const shell = require('shelljs');
+const template = require('./../template/componentTemplate');
+const fs = require('fs-extra');
+const replace = require('replace');
+const colors = require('colors');
 
 let nofolder = false;
 let functional = false;
@@ -10,26 +12,31 @@ let stylesheet = false;
 let newCompPath;
 let newContPath;
 
-export default function createComponent(component, cmd) {
+module.exports = async function createComponent(component, cmd) {
   newCompPath = component;
-  cmd.nofolder && nofolder = true;
-  cmd.functional && functional = true;
-  cmd.container && container = true;
-  cmd.observable && observable = true;
-  cmd.style && stylesheet = true;
+  cmd.nofolder ? nofolder = true : nofolder = false;
+  cmd.functional ? functional = true : functional = false;
+  // cmd.container ? container = true : functional = false;
+  cmd.observable ? observable = true : observable = false;
+  cmd.style ? stylesheet = true : stylesheet = false;
   if (fs.existsSync('./src/components')) {
-    newCompPath = `./src/components/${component}`;
-  }
-  if (fs.existsSync('./src/containers')) {
-    newContPath = `./src/containers/${component}`;
-  }
-  let template = await buildTemplateComponents();
+    fs.mkdir('./src/components', { recursive: true }, (err) => {
+      if (err) console.log(err);
+      else console.log('Folder components created');
+    });
+  }        
+  newCompPath = `./src/components/${component}`;
+
+  // if (fs.existsSync('./src/containers')) {
+  //   newContPath = `./src/containers/${component}`;
+  // }
+  let template = await buildTemplateComponent();
   writeFile(template, component, newCompPath);
-  let templateContainer = await buildTemplateComponents();
-  writeFile(templateContainer, component, newContPath);
+  // let templateContainer = await buildTemplateComponents();
+  // writeFile(templateContainer, component, newContPath);
 };
 
-function buildTemplate() {
+function buildTemplateComponent() {
   let imports = [template.imports.react, template.imports.propTypes];
   if (observable) {
     imports.push(template.imports.observable)
@@ -41,23 +48,19 @@ function buildTemplate() {
   let exported = observable ? [template.exported.observable] : [template.exported.default];
   return imports.join('\n') + '\n' + body + '\n' + exported;
 }
+
 function capitalize(comp) {
   return comp[0].toUpperCase() + comp.substring(1, comp.length);
 }
+
 function writeFile(template, component, path) {
   if (nofolder) {
     strArr = newCompPath.split('/');
     strArr.splice(strArr.length - 1, 1);
     path = strArr.join('/');
-    console.log(path);
   }
   let comp = component.split('/');
   comp = comp[comp.length - 1];
-  if (path) {
-    path = path + '/' + capitalize(comp);
-  } else {
-    path = capitalize(comp);
-  }
   if (stylesheet) {
     if (!fs.existsSync(`${path}.scss`)) {
       console.log('creating syles');
